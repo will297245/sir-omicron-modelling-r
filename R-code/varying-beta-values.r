a0729e77d2b9e0f22ba57c0e4ace16c8eb73e740
will297245 <willlodder1@gmail.com>
2026-07-29 15:21:36 +0100

    Delete R-code directory

--- R-code/varying-beta-values.r
+++
@@ -1,7 +0,0 @@
-function_factory_beta <- function(gamma, x0, y0, T, h) {
-    function(beta) {
-        calculate_infectious_change(beta, gamma, x0, y0, T, h)
-    }
-}
-
-beta_variable_function <- function_factory_beta(0.2, 0.9, 0.1, 40, 0.1)
