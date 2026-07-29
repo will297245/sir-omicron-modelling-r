8148601ea1494abed0b7b5cdba2bc073caa672a5
will297245 <willlodder1@gmail.com>
2026-07-29 14:36:12 +0100

    Update varying-beta-values.r

--- R-code/varying-beta-values.r
+++ R-code/varying-beta-values.r
@@ -5,7 +5,3 @@ function_factory_beta <- function(gamma, x0, y0, T, h) {
 }
 
 beta_variable_function <- function_factory_beta(0.2, 0.9, 0.1, 40, 0.1)
-
-beta_0_5 <- beta_variable_function(0.5)
-beta_0_6 <- beta_variable_function(0.6)
-beta_0_7 <- beta_variable_function(0.7)
