93680d99c449bc21e62f5d4868e5c61d525c835d
will297245 <willlodder1@gmail.com>
2026-07-29 13:51:49 +0100

    Update Markov-simulation.r

--- R-code/Markov-simulation.r
+++ R-code/Markov-simulation.r
@@ -38,3 +38,74 @@ simulate_markov <- function(beta, gamma, mu, x0, y0, v0, T, N = 56490048) {
   
   return(df)
 }
+
+
+
+
+
+
+
+simulate_many_markov <- function(M, beta, gamma, mu, x0, y0, v0, T, N_eff) {
+  
+  simulations <- vector("list", M)
+  
+  for (m in 1:M) {
+    
+    sim <- simulate_markov(
+      beta = beta,
+      gamma = gamma,
+      mu = mu,
+      x0 = x0,
+      y0 = y0,
+      v0 = v0,
+      T = T,
+      N = N_eff
+    )
+    
+    # Remove first artificial incidence value
+    sim <- sim[-1, ]
+    
+    simulations[[m]] <- data.frame(
+      simulation = m,
+      t = sim$t,
+      incidence_prop = sim$incidence / N_eff
+    )
+  }
+  
+  do.call(rbind, simulations)
+}
+
+
+set.seed(123)
+
+M <- 5000
+T_fit <- nrow(omicron_data)
+
+
+
+
+many_sims <- simulate_many_markov(
+  M = M,
+  beta = beta_best_markov,
+  gamma = 0.1,
+  mu = 0,
+  x0 = 1 - 0.004,
+  y0 = 0.004,
+  v0 = 0,
+  T = T_fit,
+  N_eff = N_eff
+)
+
+
+
+
+simulation_summary <- do.call(
+  rbind,
+  lapply(split(many_sims$incidence_prop, many_sims$t), function(z) {
+    data.frame(
+      mean = mean(z),
+      lower = as.numeric(quantile(z, 0.025)),
+      upper = as.numeric(quantile(z, 0.975))
+    )
+  })
+)
