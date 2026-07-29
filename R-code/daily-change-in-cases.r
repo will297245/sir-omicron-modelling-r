1670a19a5e98abf3a854cb1568186bb8aeb5c152
will297245 <willlodder1@gmail.com>
2026-07-29 14:36:40 +0100

    Update daily-change-in-cases.r

--- R-code/daily-change-in-cases.r
+++ R-code/daily-change-in-cases.r
@@ -1,5 +1,5 @@
 incidence <- function(beta, gamma, x0, y0, T, h) {
-    model_output <- euler(beta, gamma, x0, y0, T, h)
+    model_output <- simulate_sir_euler(beta, gamma, x0, y0, T, h)
 
     #adding 0 to the start of it to make the vector lengths natch up to t vector
     incidence_values <- c(0, diff(model_output$y))
