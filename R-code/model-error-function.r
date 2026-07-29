95938731777be4fd9bb9d91f15892965fb3141f6
will297245 <willlodder1@gmail.com>
2026-07-29 13:41:38 +0100

    Create model-error-function.r

---
+++ R-code/model-error-function.r
@@ -0,0 +1,18 @@
+error <- function(beta, gamma, x0, y0, data) {
+    #largest time value in data
+    T <- data$time[nrow(data)]
+    #assuming equal step size
+    h <- data$time[2] - data$time[1]
+
+    #incidence from data set
+    dt <- data$incidence
+    #estimated incidence from previous  function
+    mt <- incidence(beta, gamma, x0, y0, T, h)$incidence
+
+    #checking lengths of mt and dt are same
+    if (length(mt) != length(dt)) {
+        stop("Model output and data incidence have different lengths.")
+    }
+
+    sum((mt - dt)^2)
+}
